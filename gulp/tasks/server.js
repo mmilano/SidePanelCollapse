f4f754189fd8c85b107409dc371ba4b1aaadd50b
mmilano <mmilano+git@gmail.com>
2019-02-03 15:09:55 -0800

    revise & cleanup the server task

--- gulp/tasks/server.js
+++ gulp/tasks/server.js
@@ -1,4 +1,7 @@
 // gulp task file
+//
+// simple node server for dev
+// along with simple utility function to show current IP address
 
 const gulp =          require("gulp");
 
@@ -10,36 +13,29 @@ const networkInterfaces = require("os").networkInterfaces();
 
 const siteBuildRoot = "./build/";
 
-// figure out the current IP adress
-// and then use that for configuring the webserver
 
-// networkinterfaces:
-// en0: wifi
-// en4: ethernet
-
-let currentAddress;
-let ip_main, ip;
-
-if (typeof networkInterfaces["en4"] !== "undefined") {
-    ip_main = networkInterfaces["en4"];  // ethernet cable
-    // console.log ("ip_eth: ", ip_eth);
-    ip = ip_main.find(netInterface => netInterface.family === "IPv4");
-    currentAddress = ip.address;
-} else if (typeof networkInterfaces["en0"] !== "undefined") {
-    ip_main = networkInterfaces["en0"];  // wifi
-    ip = ip_main.find(netInterface => netInterface.family === "IPv4");
-    currentAddress = ip.address;
+function findAddress() {
+    let ip_main, ip, address;
+    if (typeof networkInterfaces["en4"] !== "undefined") {
+        ip_main = networkInterfaces["en4"];  // ethernet cableconsole.log ("ip_eth: ", ip_eth);
+        ip = ip_main.find(netInterface => netInterface.family === "IPv4");
+        address = ip.address;
+    } else if (typeof networkInterfaces["en0"] !== "undefined") {
+        ip_main = networkInterfaces["en0"];  // wifi
+        ip = ip_main.find(netInterface => netInterface.family === "IPv4");
+        address = ip.address;
+    };
+    return address;
 }
 
-// console.log ("currentAddress:", currentAddress);
+let currentAddress = findAddress();
+console.log ("***** current IP address:", currentAddress);
 
 
-// webserver
-// simple node server for dev
-
-// note: host 0.0.0.0 appears to allow
-// localhost from mac, and 192.168.1.2 from a virtual win10 machine.
-// original source:
+// note: host 0.0.0.0 allows a browser to view the built site at
+// http://localhost:9191 OR http://[current IP address]:9191 (on macOS),
+// & http://[current IP address]:9191 from a virtual (win10) machine.
+// original source of this suggestion:
 // https://stackoverflow.com/questions/10158771/access-localhost-on-the-main-machine-from-vmware-workstation-8-for-asp-net-devel/10159420#10159420
 
 const serverOptions = {
@@ -54,37 +50,9 @@ const serverOptions = {
     },
 };
 
-gulp.task("webserver", function webserver(done) {
+function webserver(done) {
     connect.server(serverOptions);
     done();
-});
+}
 
-
-// const browseSyncOptions = {
-//     port: 9191,
-//     ui: {
-//         port: 8181
-//     },
-//     server: {
-//         baseDir: siteBuildRoot,
-//         serveStaticOptions: {
-//             extensions: ["html"]
-//         }
-//     },
-//     logLevel: "silent",
-//     logFileChanges: false,
-//     online: true,
-//     // Don't show any notifications in the browser
-//     notify: false,
-//     // Wait for a specified window of event-silence before sending any reload events
-//     reloadDebounce: 200,
-//     // Don't append timestamps to injected files
-//     timestamps: false
-// };
-
-// gulp.task('browser-sync', function(done) {
-//     browserSync.init ({
-//         browseSyncOptions
-//     });
-//     done();
-// });
+gulp.task("webserver", webserver);
