d44ce295e037c6dc3f8effbcafc39344258d629f
mmilano <mmilano+git@gmail.com>
2019-02-03 15:10:49 -0800

    move the server sub-task into the main gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -40,12 +40,15 @@ const del =             require("del");
 const node_path =       require("path");
 const fs_utimes =       require("fs").utimes;
 
+// dev webserver
+const connect =         require("gulp-connect");
+const networkInterfaces = require("os").networkInterfaces();
+
+
 const requireDir =      require("require-dir");
 
-
-
 // pull in all of the subtasks
-requireDir("./gulp/tasks/");
+// requireDir("./gulp/tasks/");
 
 
 // simply touch a file so that filesystem thinks the file changed
@@ -166,6 +169,54 @@ function siteClean(done) {
 }
 
 gulp.task("site:clean", siteClean);
+
+
+// webserver
+// simple node server for dev
+// along with simple utility function to show current IP address
+
+function findAddress() {
+    let ip_main, ip, address;
+    if (typeof networkInterfaces["en4"] !== "undefined") {
+        ip_main = networkInterfaces["en4"];  // ethernet cableconsole.log ("ip_eth: ", ip_eth);
+        ip = ip_main.find(netInterface => netInterface.family === "IPv4");
+        address = ip.address;
+    } else if (typeof networkInterfaces["en0"] !== "undefined") {
+        ip_main = networkInterfaces["en0"];  // wifi
+        ip = ip_main.find(netInterface => netInterface.family === "IPv4");
+        address = ip.address;
+    };
+    return address;
+}
+
+// note: host 0.0.0.0 allows a browser to view the built site at
+// http://localhost:9191 OR http://[current IP address]:9191 (on macOS),
+// & http://[current IP address]:9191 from a virtual (win10) machine.
+// original source of this suggestion:
+// https://stackoverflow.com/questions/10158771/access-localhost-on-the-main-machine-from-vmware-workstation-8-for-asp-net-devel/10159420#10159420
+
+const serverOptions = {
+    name: "dev",
+    port: 9191,
+    host: "0.0.0.0",
+    defaultFile: "index.html",
+    root: siteBuildDestinationRoot,
+    directoryListing: {
+      enable: false,
+      path: "./"
+    },
+};
+
+function webserver(done) {
+
+    connect.server(serverOptions);
+    let currentAddress = findAddress();
+    console.log ("***** current IP address:", currentAddress);
+    done();
+}
+
+gulp.task("webserver", webserver);
+
 
 
 // copy images
