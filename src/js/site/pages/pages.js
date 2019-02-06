33d29ad1e28ca4fc42dfd44e5ba379f9b0bb74cf
mmilano <mmilano+git@gmail.com>
2019-02-06 15:57:48 -0800

    cleaning up. revise some comments
    why didnt the page-url method declaration throw an error?

--- src/js/site/pages/pages.js
+++ src/js/site/pages/pages.js
@@ -13,7 +13,7 @@
 // set of internal utility methods
 var util = {
 
-    checkWidth: function checkWidth() {
+    checkWidth: function() {
         // Bootstrap values are included as css variables in the Bootstrap css.
         // get the value
         let breakpoint_md = parseInt(getComputedStyle(document.body).getPropertyValue("--breakpoint-md"));
@@ -28,19 +28,18 @@ var util = {
         return (pageWidth > breakpoint_md ? true : false);
     },
 
-    scrollSpyCreate: function scrollSpyCreate(target, flag) {
-
-        // if a target value is passed, use that; if not, use the default value
+    scrollSpyCreate: function(target, flag) {
+        // if a target value is passed, use that; if not, use a default value for the primary nav
         target = target ? target : "#primaryNav";
 
-        // widthFlag to check for window width and only activate scrollspy if true = in certain cases, wider window sizes
+        // check if scrollspy should activate based on the window width - in certain cases, wider window sizes
         // default to the computed active flag, whatever that is
-        var shouldSpy = (flag !== undefined) ? flag : scrollSpyShouldActivate;
+        let shouldSpy = (flag !== undefined) ? flag : scrollSpyShouldActivate;
 
         if (shouldSpy) {
-            $(document.body).scrollspy ({
+            $(document.body).scrollspy({
                 target: target,
-                offset: 140,
+                offset: 100,
             });
         };
     },
@@ -59,8 +58,9 @@ var util = {
     },
 
     getIfDisplayed: function getDisplayStyle(el) {
+        // setup closure function for the passed in element for ongoing use in page
+        // return true if the element of interest IS displayed
         return function() {
-            // return true if the element of interest (passed in as param) IS displayed
             return (getComputedStyle(el).display !== "none" ? true : false );
         };
     },
@@ -69,11 +69,11 @@ var util = {
 
 
 // flag to only activate scrollspy if window is larger than breakpoint
-// this is dependent on the layout being in columns are md size, so that nav will be displayed
+// this is dependent on the layout being in columns >= md size, so that nav will be displayed
 let scrollSpyShouldActivate = util.checkWidth();
 
 // define page specific scripts, if any are needed.
-// if not, can use the defaultPage
+// if not, use the default
 
 var pages = {
 
@@ -82,7 +82,8 @@ var pages = {
 
         // start page setup
 
-        // define the 2 different nav elements for the page
+        // define the 2 different nav's for the page
+        // for scrollspy, these need to be css selectors for the actual nav element
         const navHorizontal = "#primaryNav";
         const navVertical = "#sideNav-nav";
 
@@ -91,26 +92,27 @@ var pages = {
         // the vertical, sidenav, displayed when the primary nav is collapsed at smaller screen sizes
 
         // start by checking
-        // the primarynav horizontal element that will collapse according to screen size (css media query)
-        let hnav = document.getElementById("primaryNav-horiz");
-        // function: is the horizontal nav displayed (have a display value other than 'none')
-        let horizNavDisplayed = util.getIfDisplayed(hnav);
+        // the primarynav horizontal nav that will collapse depending to screen size (= navbar-expand-md)
+        let horizontalNav = document.getElementById("primaryNav-horiz");
+        // is the horizontal nav being displayed (i.e. have a display value other than 'none')
+        let horizontalNavDisplayed = util.getIfDisplayed(horizontalNav);
 
+        console.log ("horizontalNavDisplayed:" , horizontalNavDisplayed());
         // create the scrollspy on the nav for the current state of the homepage
         // homepage: needs to have the scrollspy work on both
         // (a) the primarynav, horizontal links when window is wider,
-        // (2) the sidenav, vertical list when window is narrower
-
-        let targetNav = horizNavDisplayed() ? navHorizontal : navVertical;
+        // (b) the sidenav, vertical list when window is narrower
+        let targetNav = horizontalNavDisplayed() ? navHorizontal : navVertical;
         // create the first scrollSpy on the currently displayed nav, and pass true because it "should spy" now.
         util.scrollSpyCreate(targetNav, true);
 
         // end of page setup
-        util.scrollSpyRefresh();
+        // util.scrollSpyRefresh();
     },
 
     // default page handler
     "default": function(pageID) {
+        console.log ("default page function", pageID);
         util.scrollSpyCreate("#tableOfContents", scrollSpyShouldActivate);
     },
 };
@@ -125,7 +127,7 @@ var pageMain = function(pageID) {
 
     console.log ("page: ", pageID);
     // if there is page function defined, invoke that
-    // if no page function defined, invoke the defaultPage function
+    // if no page function defined, invoke the default
     if (typeof site.pageMethods[pageID] !== "undefined") {
         site.pageMethods[pageID](pageID);
     } else {
@@ -135,7 +137,6 @@ var pageMain = function(pageID) {
 
 
 // attach/expose specific util methods for use elsewhere
-pages.pageURL = util.pageURL;
 pages.pageMain = pageMain;
 
 module.exports = pages;
