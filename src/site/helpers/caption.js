9bd9acbddec4cb05b4f608f7ecc820d9b1c4d090
mmilano <mmilano+git@gmail.com>
2019-02-08 10:09:56 -0800

    new caption element for the page information

---
+++ src/site/helpers/caption.js
@@ -0,0 +1,50 @@
+// *****
+// handlebars block helper module
+//
+// caption element
+
+
+const basepath = process.cwd();
+const trimWhitespace = require(basepath + "/src/site/lib/trim");
+
+module.exports = function(attr, options) {
+    "use strict";
+
+    let out, open;
+
+    const openTag =  '<div class="caption">';
+    const closeTag = "</div>";
+
+    // allow for arbitrary number of attributes passed
+    if (!options || !options.fn) {
+        options = arguments[arguments.length-1];
+    }
+
+    // if there are attributes, parse the attributes passed and construct the opening tag
+    // otherwise just use default
+    if (options.hash && Object.keys(options.hash).length > 0) {
+        let attributes = "";
+
+        // if parameter passed in is "attribute=true", handle that as a special case
+        // of a single name attribute
+        // otherwise, express as: attribute="value"
+        for (var a in (options.hash)) {
+            if (options.hash[a] === true) {
+               attributes = " " + a + attributes;
+            } else {
+                attributes = " " + a + "=\"" + options.hash[a] + "\"" + attributes;
+            }
+        }
+        open = [openTag.slice(0, (openTag.length -1)), attributes, openTag.slice(openTag.length -1)].join("");
+
+    } else {
+        open = openTag;
+    }
+
+    out = open;
+    let headingText = trimWhitespace(options.fn(this));
+    out += headingText;
+    out += closeTag;
+
+    return out;
+};
