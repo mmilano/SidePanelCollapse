5acd3124eca095638dc876c565671db019928d65
mmilano <mmilano+git@gmail.com>
2019-02-12 19:54:56 -0800

    fix comment

--- src/site/lib/trim.js
+++ src/site/lib/trim.js
@@ -7,7 +7,7 @@ var trimSpace = function(text) {
     "use strict";
 
     let temp = text.trim();  // trim front and trailing space
-    temp = temp.replace(/\s+/g, " ");  // reduce all linebreaks w/1 space
+    temp = temp.replace(/\s+/g, " ");  // replace all linebreaks w/1 space
     temp = temp.replace(/\s\s+/g, " ");  // replace all multiple spaces w/1 space
     return temp;
 };
