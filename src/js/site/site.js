607b70e865b5e512d121e32fa1288d8a47bffc6b
mmilano <mmilano+git@gmail.com>
2019-02-14 10:17:02 -0800

    change site's js to invoke the sidepanel module

--- src/js/site/site.js
+++ src/js/site/site.js
@@ -5,11 +5,26 @@
 // or functions that will be made available via the 'site." namespace, via the exports
 
 
-// incorporate the script for the sidenav
-require("sidePanel.js");
+// incorporate the script for the sidepanel
+var SidePanelCollapse = require("SidePanelCollapse.js");
+
+
+// instantiate a new sidepanel for the page
+
+// sidepanel options for this site
+var sidepanelOptions = {
+    sidepanelElement: "#sidePanel",
+    sidepanelCloseElement: ".sidepanel-close",
+
+//    durationShow: "1.1222s",
+    backdropEnabled: false,
+
+};
+
+var side = new SidePanelCollapse(sidepanelOptions);
 
 // collection of scripts and handlers specific to each page, keyed to page ID
-let pageMethods = require("pages.js");
+var pageMethods = require("pages.js");
 
 module.exports = {
     pageMethods: pageMethods,
