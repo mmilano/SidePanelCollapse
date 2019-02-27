667d38c6d41a52a9604a8a48113ff80b2b4bac62
mmilano <mmilano+git@gmail.com>
2019-02-26 17:38:04 -0800

    tweak the index page script

--- src/js/site/pages/pages.js
+++ src/js/site/pages/pages.js
@@ -13,19 +13,21 @@
 // utility methods
 var util = {
 
+    // utility method to check if window is larger than breakpoint
     checkWidth: function() {
         // Bootstrap values are included as css variables in the Bootstrap css.
         // get the value
-        let breakpoint_md = parseInt(getComputedStyle(document.body).getPropertyValue("--breakpoint-md"));
+        let breakpoint_size = parseInt(getComputedStyle(document.body).getPropertyValue("--breakpoint-lg"));
+        if (!breakpoint_size) {
+            // if no value, then the browser doesn't support css variables.
+            // so use an arbitrary default value
+            breakpoint_size = 42;
+        };
+
         // get width of the current window
         let pageWidth = window.innerWidth;
 
-        if (!breakpoint_md) {
-            // if no value, then the browser doesn't support css variables. e.g. IE 10.
-            // so use a default value for now
-            breakpoint_md = 42;
-        };
-        return (pageWidth > breakpoint_md ? true : false);
+        return (pageWidth > breakpoint_size ? true : false);
     },
 
     scrollSpyCreate: function(target, flag) {
@@ -34,7 +36,7 @@ var util = {
 
         // check if scrollspy should activate based on the window width - in certain cases, wider window sizes
         // default to the computed active flag, whatever that is
-        let shouldSpy = (flag !== undefined) ? flag : scrollSpyShouldActivate;
+        let shouldSpy = (flag !== undefined) ? flag : util.checkWidth();
 
         if (shouldSpy) {
             $(document.body).scrollspy({
@@ -57,7 +59,7 @@ var util = {
         util.scrollSpyCreate(target);
     },
 
-    getIfDisplayed: function getDisplayStyle(el) {
+    checkIfDisplayed: function getDisplayStyle(el) {
         // setup closure function for the passed in element for ongoing use in page
         // return true if the element of interest IS displayed
         return function() {
@@ -69,7 +71,7 @@ var util = {
 
 
 // flag to only activate scrollspy if window is larger than breakpoint
-// this is dependent on the layout being in columns >= md size, so that nav will be displayed
+// this is dependent on the layout being in columns >= checked size, so that nav will be displayed
 let scrollSpyShouldActivate = util.checkWidth();
 
 // define page specific scripts, if any are needed.
@@ -81,19 +83,19 @@ var pages = {
     "index": function indexPage(pageID) {
 
         // define the 2 different nav's for the page
+        // the horizontal, primarynav, displayed at wider sizes
+        // the vertical, sidenav, displayed when the primary nav is collapsed at smaller screen sizes
+        //
         // for scrollspy, these need to be css selectors for the <nav> element
         const navHorizontal = "#primaryNav";
         const navVertical = "#sidepanelNav";
 
         // deal with the 2 possible primary navs on the page:
-        // the horizontal, primarynav, displayed at wider sizes
-        // the vertical, sidenav, displayed when the primary nav is collapsed at smaller screen sizes
-
         // start by checking
-        // the primarynav horizontal nav that will collapse depending to screen size (= navbar-expand-md)
+        // the primarynav horizontal nav that will collapse depending to screen size (= navbar-expand-*)
         let horizontalNav = document.getElementById("primaryNav-horiz");
-        // is the horizontal nav being displayed (i.e. have a display value other than 'none')
-        let horizontalNavDisplayed = util.getIfDisplayed(horizontalNav);
+        // is the horizontal nav being displayed (i.e. have a display value other than 'none'?)
+        let horizontalNavDisplayed = util.checkIfDisplayed(horizontalNav);
 
         console.log ("horizontalNavDisplayed:" , horizontalNavDisplayed());
         // create the scrollspy on the nav for the current state of the homepage
@@ -101,8 +103,19 @@ var pages = {
         // (a) the primarynav, horizontal links when window is wider,
         // (b) the sidenav, vertical list when window is narrower
         let targetNav = horizontalNavDisplayed() ? navHorizontal : navVertical;
-        // create the first scrollSpy on the currently displayed nav, and pass true because it "should spy" now.
+        // create the first scrollSpy on the currently displayed nav, and it "should spy" now.
         util.scrollSpyCreate(targetNav, true);
+
+        // instantiate a new sidepanel for the page
+        // sidepanel options for this site
+        var sidepanelOptions = {
+
+            durationHide: "2s",
+            durationHideFast: "5s",
+            backdropStyle: "dark",
+        };
+        var sidepanel = new SidePanelCollapse(sidepanelOptions);
+
     },
 
     // default page handler
@@ -116,13 +129,14 @@ var pages = {
             sidepanelCloseElement: ".sidepanel-close",
 
             durationHide: "4s",
-            durationHideFast: "9s",
+            durationHideFast: "5s",
             backdropStyle: "dark",
         };
 
         var sidepanel = new SidePanelCollapse(sidepanelOptions);
 
-        util.scrollSpyCreate("#tableOfContents", scrollSpyShouldActivate);
+        // only need TOC scrollspy if window is above certain size
+        util.scrollSpyCreate("#tableOfContents", util.checkWidth());
     },
 };
 
