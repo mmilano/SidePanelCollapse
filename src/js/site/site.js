98c58b0afdf1d5e1617c03d274c83a6c2a412287
mmilano <mmilano+git@gmail.com>
2019-02-26 17:39:14 -0800

    move the sidepanel creation out of this script and into the pages script

--- src/js/site/site.js
+++ src/js/site/site.js
@@ -1,27 +1,13 @@
 // site script: main entry point
-// general site javascript initialization and misc
-// defines globals,
+// general site javascript initialization
+//
+// globals,
 // immediate execution functions,
-// or functions that will be made available via the 'site." namespace, via the exports
+// or functions that will be available to the site in general
 
 
-// incorporate the script for the sidepanel
-var SidePanelCollapse = require("SidePanelCollapse.js");
-
-
-// instantiate a new sidepanel for the page
-
-// sidepanel options for this site
-var sidepanelOptions = {
-    sidepanelElement: "#sidePanel",
-    sidepanelCloseElement: ".sidepanel-close",
-
-//    durationShow: "1.1222s",
-    backdropEnabled: true,
-
-};
-
-var side = new SidePanelCollapse(sidepanelOptions);
+// make sidepanel available as global
+window.SidePanelCollapse = require("SidePanelCollapse.js");
 
 // collection of scripts and handlers specific to each page, keyed to page ID
 var pageMethods = require("pages.js");
