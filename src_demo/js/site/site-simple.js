6c3991d0bda18fd984c24f3fd83696ad7689d228
mmilano <mmilano+git@gmail.com>
2019-03-05 11:50:32 -0800

    add a full list of defaults for testing, but all commented out

--- src_demo/js/site/site-simple.js
+++ src_demo/js/site/site-simple.js
@@ -5,10 +5,21 @@
 /* globals SidePanelCollapse */    // jshint: global values
 "use strict";
 
-var sidepanelOptions = {
-    sidepanelElement: "#sidePanel",
-    sidepanelCloseElement: ".sidePanel-close",
-};
-// initialize and instantiate a new sidepanel for the page
-var sidepanel = new SidePanelCollapse();
+var sidepanelOptions;
 
+sidepanelOptions = {
+//     sidepanelElement: "#sidePanel",
+//     sidepanelCloseElement: ".sidePanel-close",
+//
+//     durationShow: "2s",
+//     durationHide: "1.5s",
+//     durationHideFast: "0.25s",
+//
+//     backdropEnabled: true,
+//     backdropStyle: "light",
+//
+//     sidePanelIsOpenClass: "sidepanel-shown",
+};
+
+// initialize and instantiate a new sidepanel for the page
+var sidepanel = new SidePanelCollapse(sidepanelOptions);
