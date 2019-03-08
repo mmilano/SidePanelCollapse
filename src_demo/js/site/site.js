01ad63451328a96bca9ec492608fd51f3df0832e
mmilano <mmilano+git@gmail.com>
2019-03-07 16:31:39 -0800

    change the reference to the minified version

--- src_demo/js/site/site.js
+++ src_demo/js/site/site.js
@@ -7,7 +7,7 @@
 
 
 // make sidepanel available as global
-window.SidePanelCollapse = require("SidePanelCollapse.js");
+window.SidePanelCollapse = require("SidePanelCollapse.min.js");
 
 // collection of scripts and handlers specific to each page, keyed to page ID
 var pageMethods = require("pages.js");
