77821ce047d8f78de450e0d42f3c4ada1dfaccbe
mmilano <mmilano+git@gmail.com>
2019-03-13 01:08:42 -0700

    changing the simple demo script sample to just output some messages

--- src_demo/js/site/site-simple.js
+++ src_demo/js/site/site-simple.js
@@ -1,26 +1,36 @@
 // simple demo site script: main entry point
 // general site javascript initialization
 //
-// there isn't much to this, just initialization of the sidepanel
+// there isn't much to this, just some placeholders to stand in for some script code on a page
 
 /* globals SidePanelCollapse */    // jshint: global values
 "use strict";
 
-var sidepanelOptions;
 
-// sidepanelOptions = {
-//     sidepanelElement: "#sidePanel",
-//     sidepanelCloseElement: ".sidePanel-close",
-//
-//     durationShow: "2s",
-//     durationHide: "1.5s",
-//     durationHideFast: "0.25s",
-//
-//     backdrop: true,
-//     backdropStyle: "light",
-//
-//     sidePanelIsOpenClass: "sidepanel-shown",
-// };
+// run after page is fully loaded
+window.addEventListener("load", function() {
+    var sidepanelOptions;
 
-// initialize and instantiate a new sidepanel for the page
-var sidepanel = new SidePanelCollapse(sidepanelOptions);
+    // sidepanelOptions = {
+    //     sidepanelElement: "#sidePanel",
+    //     sidepanelCloseElement: ".sidePanel-close",
+    //
+    //     durationShow: "2s",
+    //     durationHide: "1.5s",
+    //     durationHideFast: "0.25s",
+    //
+    //     backdrop: true,
+    //     backdropStyle: "light",
+    //
+    //     sidePanelIsOpenClass: "sidepanel-shown",
+    // };
+
+    // initialize and instantiate a new sidepanel for the page
+    // var sidepanel = new SidePanelCollapse(sidepanelOptions);
+
+    console.log("script does some programmatic things here after the window 'load' event fires");
+    console.log("at this point, the SidePanel will have been created and initialized.");
+});
+
+console.log("script does some programmatic things here when the script loads");
+
