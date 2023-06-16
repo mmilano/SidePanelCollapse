344be63795b741531d796eec80c753d57ebb2e43
mmilano <mmilano+git@gmail.com>
2023-06-15 17:04:48 -0700

    overhaul to change camelcase of sidePanel (from sidepanel)

--- src_demo/js/site/pages/pages.js
+++ src_demo/js/site/pages/pages.js
@@ -90,15 +90,15 @@ const pages = {
 
         // instantiate a new side panel for the page for when/if the sidenav will display
         // options for this page
-        const sidepanelOptions = {
+        const sidePanelOptions = {
             durationShow: "1.25s",
             durationHide: "1s",
             durationHideFast: "0.5s",
             backdropStyleClass: "dark",
         };
 
-        // expose sidepanel as global for demo purposes
-        window.sidepanel = new SidePanelCollapse(sidepanelOptions);
+        // expose sidePanel as global for demo purposes
+        window.sidePanel = new SidePanelCollapse(sidePanelOptions);
 
         // define the 2 different nav's for the page
         // the horizontal - primaryNav - displayed at very large window sizes (Bootstrap definition of large)
@@ -113,7 +113,6 @@ const pages = {
         // the primarynav horizontal nav that will collapse depending to screen size (= navbar-expand-*)
         const horizontalNav = document.getElementById("primaryNav-horiz");
         // is the horizontal nav being displayed (i.e. have a display value other than 'none'?)
-        // debugger;
         const horizontalNavDisplayed = util.checkIfDisplayed(horizontalNav);
         let horizontalNavIsDisplayed = util.checkIfDisplayed(horizontalNav)();
 
@@ -149,17 +148,17 @@ const pages = {
     default: function (pageID) {
         // can do something with pageID if necessary
 
-        // instantiate a new sidepanel for the page
+        // instantiate a new sidePanel for the page
         // with options (different from index)
-        const sidepanelOptions = {
+        const sidePanelOptions = {
             durationShow: "1.25s",
             durationHide: ".7s",
             durationHideFast: "0.2s",
             backdropStyle: "dark",
         };
 
-        // expose sidepanel as global for demo purposes
-        window.sidepanel = new SidePanelCollapse(sidepanelOptions);
+        // expose sidePanel as global for demo purposes
+        window.sidePanel = new SidePanelCollapse(sidePanelOptions);
 
         // only activate scrollspy on TOC if window is above certain size
         // this is dependent on the layout being in columns >= checked size, so that TOC will be displayed
--- src_demo/js/site/site-simple.js
+++ src_demo/js/site/site-simple.js
@@ -11,17 +11,17 @@ window.addEventListener("load", function() {
     // if the desire is to initialize SidePanelCollapse manually instead of the auto-init that the data-attribute initiates,
     // then this is the place that could be done, like the following (disabled) example code.
 
-    // const sidepanelOptions = {
-    //     sidepanelElement: "#sidePanel",
-    //     sidepanelCloseElement: ".sidePanel-close",
+    // const sidePanelOptions = {
+    //     sidePanelElement: "#sidePanel",
+    //     sidePanelCloseElement: ".sidePanel-close",
     //     durationShow: "2s",
     //     durationHide: "1.5s",
     //     durationHideFast: "0.25s",
     //     backdrop: true,
     //     backdropStyleClass: "dark",
-    //     sidePanelIsOpenClass: "sidepanel-shown",
+    //     sidePanelIsOpenClass: "sidePanel-shown",
     // };
-    // window.sidepanel = new SidePanelCollapse(sidepanelOptions);
+    // window.sidePanel = new SidePanelCollapse(sidePanelOptions);
 
     console.log("script can do programmatic things here after the window 'load' event fires");
     console.log("at this point, the SidePanel will have been created and initialized.");
