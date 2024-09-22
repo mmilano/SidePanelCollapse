19347f854a596ffa58b4d5aef60ade2cbafe3fe7
mmilano <mmilano+git@gmail.com>
2024-09-21 20:55:00 -0700

    new build of the library

--- dist/js/SidePanelCollapse.js
+++ dist/js/SidePanelCollapse.js
@@ -1,10 +1,10 @@
 /*! **********
- * SidePanelCollapse v1.4.0
+ * SidePanelCollapse v1.5.0
  * A Bootstrap 4-based sidebar augmenting the "collapse" component to collapse horizontally,
  * and allow variable duration timings for the transitions
  *
  * Michel Milano
- * 2023
+ * 2024
  * MIT License
  */
 
@@ -34,7 +34,7 @@
   var _proto = SidePanelCollapse.prototype;
 
   /**
-   * default selector for creation via data attribute
+   * default selector for creation of a sidepanel in the page via data attribute
    * @const
    */
   var data_selector = "[data-sidepanel-collapse]";
@@ -48,10 +48,18 @@
 
   /**
   * default value
-  * access the css variable values as module is instantiated
+  * access the css variable values as the element is instantiated
   * @const
   */
   var styles = el ? getComputedStyle(el) : "";
+
+  /**
+  * sidePanel closing duration built in choices
+  * "normal" = regular close speed
+  * "fast"   = faster closing speed
+  */
+  var sidePanelCloseDuration_normal = "normal";
+  var sidePanelCloseDuration_fast = "fast";
 
   /**
   * sidePanel default settings
@@ -67,6 +75,8 @@
   * @property {boolean} handleLinks - whether or not links are to be processed
   */
 
+  // defaults
+  // these values can be overridden on intialization
   var defaults = {
     // css selectors:
     // default selectors for the sidePanel DOM elements
@@ -125,8 +135,8 @@
     // return function. used for the link eventListener
     return function (e) {
       e.preventDefault();
-      this.closeType = "fast";
-      // create link event handler with closure
+      this.closeType = sidePanelCloseDuration_fast;
+      // create link event handler
       this.$sidePanel.one("hidden.bs.collapse", linkEvent(e.target.href));
       this.close();
     };
@@ -218,7 +228,7 @@
     if (this.isCollapsing() && !this.closeQueued) {
       // not already queued, so queue up to close immediately when transition has finished, and return out.
       this.closeQueued = true;
-      this.closeType = "fast";
+      this.closeType = sidePanelCloseDuration_fast;
       this.$sidePanel.one("shown.bs.collapse", this.close);
       return;
     } else if (this.isCollapsing() && this.closeQueued) {
@@ -249,9 +259,10 @@
       // case "page":
       // dev todo: future expansion
       /* falls through */
-      case "fast":
+      // case "fast":
+      case sidePanelCloseDuration_normal:
         _duration = this.settings.durationHideFast;
-        this.closeType = "normal"; // reset
+        this.closeType = sidePanelCloseDuration_normal; // reset
         break;
       default:
         // = "normal"
@@ -272,14 +283,6 @@
     document.removeEventListener("keyup", this.handleKey);
     document.body.classList.remove(this.settings.sidePanelIsOpenClass);
   };
-
-  // future home of method to dispose of all the sidepanel.
-  // dev note: currently disabled. to be developed.
-  //     _proto.dispose = function() {
-  //         for (var prop in this) {
-  //             this[prop] = null;
-  //         }
-  //     };
 
   // *****
   // Backdrop
@@ -352,9 +355,10 @@
      */
     function defineSettings(defaults, options) {
       // start with the defaults
-      var settings = Object.assign({}, defaults);
+      // let settings = Object.assign({}, defaults);
       // reconcile with any provided options that will supercede/overwrite defaults
-      Object.assign(settings, options);
+      // Object.assign(settings, options);
+      var settings = Object.assign(defaults, options);
 
       // if backdrop is anything other than "true", it is false
       settings.backdrop = settings.backdrop === true ? true : false;
@@ -434,7 +438,7 @@
       this.sidePanelCloseButton.addEventListener("click", this.close, false);
     } else {
       // no close button found :(
-      // the sidepanel will be initialized, but maybe this isn't what is desired?
+      // the sidepanel will be initialized, but log warning in case this isn't what is desired?
       console.warn('SidePanel: no close element could be found with the selector "' + _settings.sidePanelCloseElement + '".');
     }
 
@@ -447,7 +451,7 @@
     }
 
     // flag for which close type, and therefore duration, to use: normal, or fast
-    this.closeType = "normal"; // default behavior when closing the sidepanel
+    this.closeType = sidePanelCloseDuration_normal; // default behavior when closing the sidepanel
 
     // handle links:
     // find all the links in the sidepanel and add an event on them
